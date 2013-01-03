87106143cbfa3941e8755c4a1989ea528745f1d4
Yanick Landry <yanick.landry@gmail.com>
2013-01-02 23:00:37 -0500

    alert : better error management

--- app/controllers/spree/availability_alerts_controller.rb
+++ app/controllers/spree/availability_alerts_controller.rb
@@ -3,15 +3,21 @@ class Spree::AvailabilityAlertsController < Spree::BaseController
   def index
     if(spree_current_user)
       @products = spree_current_user.products
+    else
+      flash[:error] = t(:alert_must_be_logged)
+      redirect_to '/'
     end
   end
   
   def create
+    @product = Spree::Product.find(params[:product])
     if(spree_current_user)
-      @product = Spree::Product.find(params[:product])
       @product.users << spree_current_user if((@product.users & [spree_current_user]).empty?)
       @product.save
       redirect_to '/availability_alerts/'
+    else
+      flash[:error] = t(:alert_must_be_logged)
+      redirect_to @product
     end
   end
   
@@ -19,8 +25,11 @@ class Spree::AvailabilityAlertsController < Spree::BaseController
     if(spree_current_user)
       @product = Spree::Product.find(params[:product])
       spree_current_user.products.delete(@product)
+      redirect_to '/availability_alerts/'
+    else
+      flash[:error] = t(:alert_must_be_logged)
+      redirect_to '/'
     end
-    redirect_to '/availability_alerts/'
   end
   
   def self.list_with_product(product)
